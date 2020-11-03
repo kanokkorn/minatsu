1dbb6b1e475f07cd769fd3091cc865e5518988fd
kanokkorn <kanokorn@outlook.jp>
2020-11-03 08:32:57 +0700

    add gtkwebkit

--- minatsu.c
+++ minatsu.c
@@ -1,40 +1,34 @@
 #include "minatsu.h"
 
-typedef struct{
-  GtkWidget *w_webkit_webview;
-}app_widgets;
-
-static void greeting(GtkWidget *widget, gpointer data){
-  g_print("Hello minatsu\n");
+static void destroyWin(GtkWidget* widget){
+  gtk_main_quit();
 }
 
-static void activate (GtkApplication* app, gpointer user_data){
-
-  GtkWidget *window;
-  GtkWidget *button;
-  GtkWidget *button_box;
-
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW (window), "minatsu");
-  gtk_window_set_default_size(GTK_WINDOW (window), 1280, 720);
-
-  button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add(GTK_CONTAINER (window), button_box);
-
-  button = gtk_button_new_with_label("Say");
-  g_signal_connect(button, "clicked", G_CALLBACK(greeting), NULL);
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
-  gtk_container_add(GTK_CONTAINER (button_box), button);
-
-  gtk_widget_show_all (window);
+static void greeting(void){
 }
 
-int main(int argc, char **argv){
-  GtkApplication *app;
-  int status;
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref(app);
-  return status;
+static gboolean closeWeb(WebKitWebView* webView, GtkWidget* window){
+  gtk_widget_destroy(window);
+  return TRUE;
+}
+
+int main(int argc, char* argv[]){
+  GtkWidget *win;
+  WebKitWebView *web;
+  gchar *url = "./index.html";
+  gtk_init(&argc, &argv);
+  if(argc == 2){
+    url = argv[1];
+  }
+  win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size(GTK_WINDOW(win), 1280, 720);
+  web = WEBKIT_WEB_VIEW(webkit_web_view_new());
+  gtk_container_add(GTK_CONTAINER(win), GTK_WIDGET(web));
+  g_signal_connect(win, "destroy", G_CALLBACK(destroyWin), NULL);
+  g_signal_connect(web, "close", G_CALLBACK(closeWeb), win);
+  webkit_web_view_load_uri(web, url);
+  gtk_widget_grab_focus(GTK_WIDGET(web));
+  gtk_widget_show_all(win);
+  gtk_main();
+  return 0;
 }
